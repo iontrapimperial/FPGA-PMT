8b5f27362adfc7791ce20afa5d46fa76c4c7fed5
Sarah Woodrow <sarah.woodrow08@imperial.ac.uk>
2013-10-23 17:10:36 +0100

    Commit up down count: working with clock, not without clock

---
+++ VerilogBasics/Up Down counter/Frequency_Divider.v
@@ -0,0 +1,19 @@
+module oscillator_count(clock, on_off);
+
+
+input clock;
+output reg on_off;
+reg [31:0] count;
+
+always @(posedge clock)
+begin
+
+if (count==32'd5000000) 
+begin count <= 16'd0;
+on_off <= !on_off;
+end
+else begin
+count<=count +1;
+end
+end
+endmodule 
